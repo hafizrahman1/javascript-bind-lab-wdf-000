d116f7d6e980fb4cb5bb1963e86e0bf6a8ddb56e
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-12-22 19:22:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+const digitalClock = { 
+  time: Math.round(Date.now() / 1000), 
+  startTicking: function() {
+    setInterval(()=> this.time++, 1000);
+  }
+}
+
+const censor = (word, str) => {
+  let matcher = new RegExp(word, 'gi');
+  return str.replace(matcher, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');
